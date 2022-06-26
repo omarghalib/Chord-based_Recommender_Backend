864d5266c0fd23568d8bc624c3a3c3ea64550396
OmarAbushaban <omarghalib@gmail.com>
2022-06-27 00:29:02 +0300

    added getSimilarSongs callable

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,38 @@
 import * as functions from "firebase-functions";
-
+import * as firebaseAdmin from "firebase-admin";
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
+firebaseAdmin.initializeApp();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+export const getSimilarSongs =
+  functions.https.onRequest((request, response) => {
+    const minSimilarityScore = request.body.minSimilarityScore;
+    const songId = request.body.songId;
+    const dbRef = firebaseAdmin.database().ref();
+    type Result = {
+      [key: string]: any;
+    };
+    const result: Result = {};
+    dbRef.once("value")
+        .then(function(snapshot) {
+          for (let index = 0; index < snapshot.numChildren(); index++) {
+            if (index != songId) {
+              console.log("index " + index);
+              const similaritiScore =
+              snapshot.val()[songId]["similarityScores"][index];
+              if (similaritiScore >= minSimilarityScore) {
+                console.log(index);
+                console.log(similaritiScore);
+                const indexString = index.toString();
+                result[indexString] =
+              {
+                "title": snapshot.val()[index]["title"],
+                "similarityScore": similaritiScore,
+              };
+              }
+            }
+          }
+          console.log("result: " + result);
+          response.send(result);
+        });
+  });
