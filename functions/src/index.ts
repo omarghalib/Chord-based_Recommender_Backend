f1cdd19548cb9dac16cbf8bd7e11674a18e55f4b
OmarAbushaban <omarghalib@gmail.com>
2022-06-28 01:16:43 +0300

    changed the request to a callable and improved it

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,60 +4,29 @@ import * as firebaseAdmin from "firebase-admin";
 // https://firebase.google.com/docs/functions/typescript
 firebaseAdmin.initializeApp();
 
-export const getSimilarSongs =
-  functions.https.onRequest(async (request, response) => {
-    const numOfSongs = request.body.numOfSongs;
-    const minSimilarityScore = request.body.minSimilarityScore;
-    const songId = request.body.songId;
-    const dbRef = firebaseAdmin.database().ref();
-    type Result = {
-      [key: string]: any;
-    };
-    const result: Result = {};
-    await dbRef.child(songId.toString()).
-        child("similarityScores").
-        orderByChild("score").
-        startAt(minSimilarityScore).
-        limitToLast(numOfSongs).
-        on("value", function(snapshot) {
-        // snapshot would have list of NODES that satisfies the condition
-        // console.log(snapshot.val());
-          console.log("-----------");
-
-          // go through each item found and print out the emails
-          snapshot.forEach(function(childSnapshot) {
-            const childData = childSnapshot.val();
-            // this will be the actual email value found
-            console.log(childData.score);
-            console.log(childData.songId);
-          });
-          result[songId.toString()] = snapshot.val();
-          response.send(result);
-        });
-    // await dbRef.child(songId.toString()).
-    //   child("similarityScores").
-    //   orderByValue().
-    //   startAt(minSimilarityScore).
-    //   on("value", async (snapshot) => {
-    //     const resultId = snapshot.val();
-    //     if (resultId != null) {
-    //       console.log("resultId " + resultId);
-    //       const title = await dbRef.
-    //         child(resultId.toString()).
-    //         child("title").once("value")
-    //         .then(function (resultSnapshot) {
-    //           console.log("title in the snapshot " + resultSnapshot.val());
-    //           return resultSnapshot.val();
-    //         });
-    //       const similarityScore = await snapshot.val();
-    //       console.log(snapshot.key);
-    //       console.log(title);
-    //       console.log(similarityScore);
-    //       result[songId.toString()] =
-    //       {
-    //         "title": await title,
-    //         "similarityScore": await similarityScore,
-    //       };
-    //     }
-    //   });
+export const getSongRecommendations =
+  functions.https.onCall((data, context) => {
+    const numOfSongs = data.numOfSongs;
+    const minSimilarityScore = data.minSimilarityScore;
+    const songId = data.songId;
+    console.log("songId " + songId);
+    return new Promise((resolve, reject) => {
+      firebaseAdmin.database().ref()
+          .child(songId).
+          child("similarityScores").
+          orderByChild("score").
+          startAt(minSimilarityScore).
+          limitToLast(numOfSongs).
+          once("value", function(snapshot) {
+            const result = snapshot.val();
+            if (result) {
+              console.log(result)
+              return resolve(result);
+            } else {
+              reject(new Error("result not found")
+              );
+            }
+          })
+          .catch(reject);
+    });
   });
